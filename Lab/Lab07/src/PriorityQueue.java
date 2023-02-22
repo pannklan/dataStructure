846ac7b9db1253dd938b48281697c76de3c01c61
pannklan <pklankasem@gmail.com>
2023-02-22 16:06:15 +0700

    Lab07

---
+++ Lab/Lab07/src/PriorityQueue.java
@@ -0,0 +1,52 @@
+public class PriorityQueue {
+	MyQueue q;
+
+	public PriorityQueue(MyQueue q) {
+		super();
+		this.q = q;
+	}
+
+	// implement this.
+	public void push(int x) throws Exception {
+	    if (q.isEmpty()) {
+	        q.insertLast(x);
+	        return;
+	    }
+	    int size = q.size();
+	    int i = 0;
+	    while (i < size && x >= q.front()) {
+	        q.insertLast(q.removeFirst());
+	        i++;
+	    }
+	    q.insertLast(x);
+	    while (i < size) {
+	        q.insertLast(q.removeFirst());
+	        i++;
+	    }
+	    //this stuck in forever loop
+//	    if (q.isEmpty()) {
+//			q.insertLast(x);
+//			return;
+//		}
+//		int p=q.front();
+//		while (x >= q.front()) {
+//			q.insertLast(q.removeFirst());
+//		}
+//		q.insertLast(x);
+//		while (q.front()>=p) {
+//			q.insertLast(q.removeFirst());
+//		}
+//		return;		
+	}
+
+	// implement this.
+	public void pop() throws Exception {
+		q.removeFirst();
+	}
+
+	// implement this
+	public int top() throws Exception {
+		return q.front();
+	}
+
+}
