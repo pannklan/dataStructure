28e400446ae8f3261e3b77ddd77ba954af7dcdb2
pannklan <pklankasem@gmail.com>
2023-04-19 11:59:47 +0700

    AVL LAB

---
+++ Lab/Lab13/src/AVLTree.java
@@ -0,0 +1,315 @@
+public class AVLTree {
+	AVLNode root;
+	int size;
+
+	public AVLTree() {
+		root = null;
+		size = 0;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public void makeEmpty() {
+		root = null;
+		size = 0;
+	}
+
+	public Iterator findMin() {
+		return findMin(root);
+	}
+
+	public Iterator findMin(AVLNode n) {
+		if (n == null)
+			return null;
+		if (n.left == null) {
+			Iterator itr = new AVLTreeIterator(n);
+			return itr;
+		}
+		return findMin(n.left);
+	}
+
+	public Iterator findMax() {
+		return findMax(root);
+	}
+
+	public Iterator findMax(AVLNode n) {
+		if (n == null)
+			return null;
+		if (n.right == null) {
+			Iterator itr = new AVLTreeIterator(n);
+			return itr;
+		}
+		return findMax(n.right);
+	}
+
+	public Iterator find(int v) {
+		return find(v, root);
+	}
+
+	public Iterator find(int v, AVLNode n) {
+		if (n == null)
+			return null;
+		if (v == n.data)
+			return new AVLTreeIterator(n);
+		if (v < n.data)
+			return find(v, n.left);
+		else
+			return find(v, n.right);
+	}
+
+	public AVLNode insert(int v) {
+		return insert(v, root, null);
+	}
+
+	// return the node n after v was added into the tree
+	public AVLNode insert(int v, AVLNode n, AVLNode parent) {
+		if (n == null) {
+			n = new AVLNode(v, null, null, parent, 0);
+			size++;
+		} else if (v < n.data) {
+			n.left = insert(v, n.left, n);
+		} else if (v > n.data) {
+			n.right = insert(v, n.right, n);
+		}
+		n = rebalance(n);
+		return n;
+	}
+
+	public AVLNode insertNoBalance(int v) {
+		return insertNoBalance(v, root, null);
+	}
+
+	private AVLNode insertNoBalance(int v, AVLNode n, AVLNode parent) {
+		if (n == null) {
+			n = new AVLNode(v, null, null, parent, 0);
+			size++;
+		} else if (v < n.data) {
+			n.left = insertNoBalance(v, n.left, n);
+		} else if (v > n.data) {
+			n.right = insertNoBalance(v, n.right, n);
+		}
+		AVLNode.updateHeight(n);
+		return n;
+	}
+
+	public AVLNode remove(int v) {
+		return remove(v, root, null);
+	}
+
+	// return the node n after v was removed from the tree
+	public AVLNode remove(int v, AVLNode n, AVLNode parent) {
+		if (n == null)
+			; // do nothing, there is nothing to be removed
+		else if (v < n.data) {
+			n.left = remove(v, n.left, n);
+		} else if (v > n.data) {
+			n.right = remove(v, n.right, n);
+		} else {
+			if (n.left == null && n.right == null) {
+				n = null;
+				size--;
+			} else if (n.left != null && n.right == null) {
+				n.left.parent = parent;
+				n = n.left;
+				size--;
+			} else if (n.right != null && n.left == null) {
+				n.right.parent = parent;
+				n = n.right;
+				size--;
+			} else {
+				AVLTreeIterator i = (AVLTreeIterator) findMin(n.right);
+				int minInRightSubtree = i.currentNode.data;
+				n.data = minInRightSubtree;
+				n.right = remove(minInRightSubtree, n.right, n);
+			}
+		}
+		n = rebalance(n);
+		return n;
+	}
+
+	public AVLNode rebalance(AVLNode n) {
+		if (n == null)
+			return n;
+		int balance = AVLNode.tiltDegree(n);
+		if (balance >= 2) {
+			if (AVLNode.tiltDegree(n.left) <= -1) // 3rd case
+				n.left = rotateRightChild(n.left);
+			n = rotateLeftChild(n); // 1st case
+		} else if (balance <= -2) {
+			if (AVLNode.tiltDegree(n.right) >= 1) // 4th case
+				n.right = rotateLeftChild(n.right);
+			n = rotateRightChild(n); // 2nd case
+		}
+		AVLNode.updateHeight(n);
+		return n;
+	}
+
+	public AVLNode rotateLeftChild(AVLNode n) {
+		AVLNode l = n.left;
+		AVLNode lr = n.left.right; // can be null
+		n.left = lr;
+		if (lr != null) {
+			lr.parent = n;
+		}
+		l.right = n;
+		l.parent = n.parent;
+		n.parent = l;
+
+		AVLNode.updateHeight(n);
+		AVLNode.updateHeight(l);
+		return l;
+	}
+
+	public AVLNode rotateRightChild(AVLNode n) {
+		AVLNode r = n.right;
+		AVLNode rl = n.right.left; // can be null
+		n.right = rl;
+		if (rl != null) {
+			rl.parent = n;
+		}
+		r.left = n;
+		r.parent = n.parent;
+		n.parent = r;
+
+		AVLNode.updateHeight(n);
+		AVLNode.updateHeight(r);
+		return r;
+	}
+
+	public void makeAVL() throws Exception {
+		//code this method
+		if (root != null) {
+            makeAVL(root);
+        }
+	}
+	
+
+private void makeAVL(AVLNode node) {
+    if (node == null) {
+        return;
+    }
+    
+    // Recursively balance the left and right subtrees
+    makeAVL(node.left);
+    makeAVL(node.right);
+
+    // Check if the node is balanced and rotate as necessary
+    int tiltDegree = AVLNode.tiltDegree(node);
+    if (tiltDegree > 1) {
+        // left subtree is too heavy
+        if (AVLNode.tiltDegree(node.left) >= 0) {
+            // left-left case: rotate right
+            node = rotateRight(node);
+        } else {
+            // left-right case: rotate left, then right
+            node.left = rotateLeft(node.left);
+            node = rotateRight(node);
+        }
+    } else if (tiltDegree < -1) {
+        // right subtree is too heavy
+        if (AVLNode.tiltDegree(node.right) <= 0) {
+            // right-right case: rotate left
+            node = rotateLeft(node);
+        } else {
+            // right-left case: rotate right, then left
+            node.right = rotateRight(node.right);
+            node = rotateLeft(node);
+        }
+    }
+
+    // Update the height of the node
+    AVLNode.updateHeight(node);
+}
+
+private AVLNode rotateLeft(AVLNode node) {
+    AVLNode child = node.right;
+    node.right = child.left;
+    if (child.left != null) {
+        child.left.parent = node;
+    }
+    child.left = node;
+    child.parent = node.parent;
+    node.parent = child;
+    AVLNode.updateHeight(node);
+    AVLNode.updateHeight(child);
+    return child;
+}
+
+private AVLNode rotateRight(AVLNode node) {
+    AVLNode child = node.left;
+    node.left = child.right;
+    if (child.right != null) {
+        child.right.parent = node;
+    }
+    child.right = node;
+    child.parent = node.parent;
+    node.parent = child;
+    AVLNode.updateHeight(node);
+    AVLNode.updateHeight(child);
+    return child;
+}
+
+	
+	public boolean isAVL() {
+		// code this method
+	    if (root == null) {
+	        return true;
+	    }
+		return isAVL(root);
+	}
+	
+	private boolean isAVL(AVLNode node) {
+	    // Check the tilt degree of the current node
+	    int tiltDegree = AVLNode.getHeight(node.left) - AVLNode.getHeight(node.right);
+	    if (tiltDegree < -1 || tiltDegree > 1) {
+	        return false;
+	    }
+
+	    // Recursively check the left and right subtrees
+	    if (node.left != null && !isAVL(node.left)) {
+	        return false;
+	    }
+	    if (node.right != null && !isAVL(node.right)) {
+	        return false;
+	    }
+
+	    // If everything is fine, return true
+	    return true;
+	}
+
+
+	public static boolean same(AVLTree t1, AVLTree t2) {
+		//code this method
+		return same(t1.root,t2.root);
+	}
+	
+	private static boolean same(AVLNode node1, AVLNode node2) {
+		if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+        return node1.data == node2.data && same(node1.left, node2.left) && same(node1.right, node2.right);
+	}
+	
+
+	public static void main(String[] args) throws Exception {
+		// example: print a tree
+
+		AVLTree t = new AVLTree();
+
+		t.root = t.insertNoBalance(33);
+		t.root = t.insertNoBalance(4);
+		t.root = t.insertNoBalance(1);
+		t.root = t.insertNoBalance(66);
+		t.root = t.insertNoBalance(2);
+		t.root = t.insertNoBalance(6);
+
+		BTreePrinter.printNode(t.root);
+
+	}
+
+}
